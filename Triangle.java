69ada188977c734fda6e0cf9b666e22416b98537
zionabebe34 <zion.abebe@post.runi.ac.il>
2023-12-21 14:27:03 +0200

    add some little changes

--- Triangle.java
+++ Triangle.java
@@ -7,7 +7,6 @@ public class Triangle {
 	public static void main(String[] args) {
 		// Put your code here
 
-
 		//define the arguments of the triangle	
 		int a = Integer.parseInt(args[0]); 
 		int b = Integer.parseInt(args[1]); 
@@ -15,7 +14,7 @@ public class Triangle {
 		// boolean var
 		boolean isTriangle = ((a + b) > c) && ((a + c) > b) && ((b + c) > a); 
      
-     	System.out.println(a + "," +  b + "," + "," + c + ":" + isTriangle);
+     	System.out.println(a + "," + " " +  b + "," + " " + c + ":" + " " + isTriangle);
 
 
     }
