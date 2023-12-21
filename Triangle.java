6905c071619aa804ffb42480d3e7d2d79472746d
zionabebe34 <zion.abebe@runi.ac.il>
2023-12-21 10:13:35 +0200

    Add awesome code for Homework 1

--- Triangle.java
+++ Triangle.java
@@ -5,15 +5,17 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		// Put your code here
+
+
+		//define the arguments of the triangle	
 		int a = Integer.parseInt(args[0]); 
 		int b = Integer.parseInt(args[1]); 
 		int c = Integer.parseInt(args[2]);
+		boolean isTriangle = ((a + b) > c) && ((a + c) > b) && ((b + c) > a); 
+     
+     	System.out.println(a + "," +  b + "," + "," + c + ":" + isTriangle);
 
-		if ( ( ( a + b )  > c)  && ( ( b + c ) > a ) ) {
-			System.out.println(true); 
-		} else{
-			System.out.println(false); 
-		}
-	}
+
+    }
 }
